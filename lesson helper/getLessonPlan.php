f19f288deda0995462fdf8a6fc31f6f860f73a8d
Christian Rogers <christian@bradensbay.com>
2024-01-09 16:49:30 -0500

    womp womp

--- lesson helper/getLessonPlan.php
+++ lesson helper/getLessonPlan.php
@@ -6,6 +6,9 @@ $N = "\n";
 $myfile = fopen("temp.txt", "w") or die("Unable to store");
 fwrite($myfile, $level.$N.$fname.$N.$week);
 fclose($myfile);
-shell_exec("python3 main.py");
+$resp = shell_exec("python3 main.py");
+$myfile = fopen("userHistory.txt", "w") or die("Unable to store");
+fwrite($myfile, $resp);
+fclose($myfile);
 header("Location: http://bradensbay.com/lessonHelper/output.html");
 ?>
